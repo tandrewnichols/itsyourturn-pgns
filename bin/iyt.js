73e62a5ef2c9a1de8e4912cdd518ec477c91835d
Andrew Nichols <tandrewnichols@gmail.com>
2015-05-27 09:15:00 -0400

    Get cartesian product

--- bin/iyt.js
+++ bin/iyt.js
@@ -2,19 +2,31 @@
 
 var program = require('commander');
 var request = require('request');
+var Combinatorics = require('js-combinatorics');
+var async = require('async');
+var util = require('util');
 
+var baseUrl = 'http://www.itsyourturn.com/pp?complgame&userid=%s&gametype=%s&tn=%s&wl=%s';
+
+// cartesianProduct(gameTyptes, tournament, winloss)
 /*
- * tn = tournament
+ * gameTypes
+ *    1: Chess Level 1
+ *    53: Intermediate Chess
+ *    60: Chess Level 2
+ *    67: Chess Level 3
+ * tournament
  *    0: regular games
  *    1: tournament games
  *    2: ladder games
- * wl = winloss
+ * winloss
  *    0: win
  *    1: loss
  *    2: draw
+ *
  * st = start (e.g. page)
  */
-var baseUrl = 'http://www.itsyourturn.com/pp?complgame&userid=%s&gametype=1&tn=%s&wl=%s';
+var product = Combinatorics.cartesianProduct([ '1', '60', '67', '53' ], [ '0', '1', '2' ], [ '0', '1', '2' ]).toArray();
 
 program.version(require('../package').version)
   .option('-u, --username <user>', 'itsyourturn.com username')
@@ -30,6 +42,12 @@ request.post('http://itsyourturn.com/iyt.dll/loginverify', {
     url: 'status?'
   }
 }, function(err, response, body) {
-  var cookie = response.headers['set-cookie'][0];
+  var cookieVal = response.headers['set-cookie'][0];
   var userid = cookie.match(/USERID=(\d+)O/)[1];
+  var jar = request.jar();
+  async.reduce(product, [], function(memo, combo, next) {
+    request.get(util.format(baseUrl, [userid].concat(combo))
+  }, function(err, games) {
+
+  });
 });
