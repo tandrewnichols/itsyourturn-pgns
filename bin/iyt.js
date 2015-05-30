5051e9edb9168691007eb1588e5cb509e0db5199
Andrew Nichols <tandrewnichols@gmail.com>
2015-05-30 18:03:20 -0400

    Get game id list (for first page only right now)

--- bin/iyt.js
+++ bin/iyt.js
@@ -5,10 +5,13 @@ var request = require('request');
 var Combinatorics = require('js-combinatorics');
 var async = require('async');
 var util = require('util');
+var cheerio = require('cheerio');
+var _ = require('lodash');
 
-var baseUrl = 'http://www.itsyourturn.com/pp?complgame&userid=%s&gametype=%s&tn=%s&wl=%s';
+var baseUrl = 'http://itsyourturn.com'
+var gamePath = '/pp?complgame&userid=%s&gametype=%s&tn=%s&wl=%s';
 
-// cartesianProduct(gameTyptes, tournament, winloss)
+// cartesianProduct(gameTypes, tournament, winloss)
 /*
  * gameTypes
  *    1: Chess Level 1
@@ -42,12 +45,50 @@ request.post('http://itsyourturn.com/iyt.dll/loginverify', {
     url: 'status?'
   }
 }, function(err, response, body) {
-  var cookieVal = response.headers['set-cookie'][0];
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+  var cookie = response.headers['set-cookie'][0];
   var userid = cookie.match(/USERID=(\d+)O/)[1];
   var jar = request.jar();
-  async.reduce(product, [], function(memo, combo, next) {
-    request.get(util.format(baseUrl, [userid].concat(combo))
-  }, function(err, games) {
-
+  jar.setCookie(request.cookie(cookie.split(';')[0]), baseUrl);
+  request = request.defaults({ jar: jar });
+  async.parallel([
+    function(next) {
+      request.get('http://itsyourturn.com/iyt.dll?userprofile?userid=' + userid + '&proftype=0', function(err, response, body) {
+        next(err, body);
+      });
+    },
+    function(next) {
+      request.get('http://itsyourturn.com/iyt.dll?userprofile?userid=' + userid + '&proftype=1', function(err, response, body) {
+        next(err, body);
+      });
+    },
+    function(next) {
+      request.get('http://itsyourturn.com/iyt.dll?userprofile?userid=' + userid + '&proftype=2', function(err, response, body) {
+        next(err, body);
+      });
+    }
+  ], function(err, results) {
+    if (err) {
+      console.log(err);
+      process.exit();
+    }
+    async.reduce(product, [], function(memo, combo, next) {
+      request.get(baseUrl + util.format.apply(util, [gamePath, userid].concat(combo)), function(err, response, body) {
+        if (err) {
+          return next(err);
+        }
+        var $ = cheerio.load(body);
+        var links = $('a[href^="/iyt.dll?a?s=7&g="]');
+        var gameIds = _.map(links, function(el) {
+          return el.attribs.href.split('&')[1].split('=')[1];
+        });
+        next(null, memo.concat(gameIds));
+      });
+    }, function(err, games) {
+      console.log(games.length);
+    });
   });
 });
